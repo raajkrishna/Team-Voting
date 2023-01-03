391fb4d746e0f3863ed088845fae0f73da663e36
raajkrishna <121881103+raajkrishna@users.noreply.github.com>
2023-01-03 07:07:26 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD3OUQDRheCpVmJrm-L6usE0v9H_ppi4lU",
+  authDomain: "team-voting-app-3925c.firebaseapp.com",
+  databaseURL: "https://team-voting-app-3925c-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-3925c",
+  storageBucket: "team-voting-app-3925c.appspot.com",
+  messagingSenderId: "359853348215",
+  appId: "1:359853348215:web:e402d04b74b1a73acafd72"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
